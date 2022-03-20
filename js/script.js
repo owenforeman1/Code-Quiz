07abce706290da06434292ee5a18552aad8f7b35
Owen Foreman <owen4man@gmail.com>
2022-03-20 19:18:22 -0400

    next question function

--- js/script.js
+++ js/script.js
@@ -5,49 +5,131 @@ var infoEl = document.createElement("div");
 
 var questionsEl = document.createElement("h2");
 var answersEl = document.createElement("ul")
+var currentQuestion = 0;
 
-var li1 = document.createElement("li");
-var li2 = document.createElement("li");
-var li3 = document.createElement("li");
-var li4 = document.createElement("li");
+var buttonOne = document.createElement("button");
+var buttonTwo = document.createElement("button");
+var buttonThree = document.createElement("button");
+var buttonFour = document.createElement("button");
 var introEl = document.createElement("p");
 
 
 // text
 h1El.textContent = "US-Citizenship-Quiz!";
 questionsEl.textContent = "1. What is one right or freedom from the First Amendment?";
-// answersEl.textContent = "";
-// li1.textContent = "Apples 🍎 ";
-// li2.textContent = "Pizza 🍕 ";
-// li3.textContent = "Dumplings 🥟 ";
-// li4.textContent = "Cupcakes 🧁 ";
 introEl.textContent = "Try to answer the following questions within the time limit. Keep in mind wrong answers will deduct scoretime by ten seconds.";
-
-var promptUser = function () {
-    introEl.remove();
-    btn.remove();
-    questionsEl.textContent =
-     "1. What is one right or freedom from the First Amendment?";
-     li1.textContent = "Apples 🍎 ";
-     li2.textContent = "Pizza 🍕 ";
-     li3.textContent = "Dumplings 🥟 ";
-     li4.textContent = "Cupcakes 🧁 ";
- body.appendChild(questionsEl);
-    body.appendChild(answersEl);
-    answersEl.appendChild(li1);
-    answersEl.appendChild(li2);
-    answersEl.appendChild(li3);
-    answersEl.appendChild(li4);    
-};
 
 body.appendChild(h1El);
 body.appendChild(introEl);
-
 body.appendChild(infoEl);
-
 h1El.setAttribute("style", "color:blue; text-align:center;");
 
+var startQuiz = function () {
+    introEl.remove();
+    btn.remove();
+    body.appendChild(questionsEl);
+    body.appendChild(answersEl);
+    answersEl.appendChild(buttonOne);
+    answersEl.appendChild(buttonTwo);
+    answersEl.appendChild(buttonThree);
+    answersEl.appendChild(buttonFour);   
+    nextQuestion(questionsArr[currentQuestion]);
+};
 
+let questionOne = {
+    title: "1. What is one right or freedom from the First Amendment?",
+    questions: ["Free pizza", "Speech", "free Coca Cola", "Free dogs"],
+    correctAnswer: 1,
+};
+
+let questionTwo = {
+    title: "2. Name one branch or part of the government?",
+    questions: ["Legislative", "New York", "Abe Lincoln", "Banana"],
+    correctAnswer: 0,
+};
+
+let questionThree = {
+    title: "3. What is one right or freedom from the First Amendment?",
+    questions: ["Free pizza", "Speech", "free Coca Cola", "Free dogs"],
+    correctAnswer: 1,
+};
+
+let questionFour = {
+    title: "3. What is one right or freedom from the First Amendment?",
+    questions: ["Free pizza", "Speech", "free Coca Cola", "Free dogs"],
+    correctAnswer: 1,
+};
+
+let questionFive = {
+    title: "3. What is one right or freedom from the First Amendment?",
+    questions: ["Free pizza", "Speech", "free Coca Cola", "Free dogs"],
+    correctAnswer: 1,
+};
+
+let questionsArr = [
+    questionOne,
+    questionTwo,
+    questionThree,
+    questionFour,
+    questionFive,
+];
+
+
+var nextQuestion = function (question) {
+// new title
+questionsEl.textContent = question.title;
+// remove excitstinbg button
+var oldButtonsArray = document.querySelectorAll("button");
+oldButtonsArray.forEach(function (element, index) {
+    element.remove();
+});
+// creating new btn
+var buttonOne = document.createElement("button");
+var buttonTwo = document.createElement("button");
+var buttonThree = document.createElement("button");
+var buttonFour = document.createElement("button");
+answersEl.appendChild(buttonOne);
+answersEl.appendChild(buttonTwo);
+answersEl.appendChild(buttonThree);
+answersEl.appendChild(buttonFour);   
+// append new button
+
+// change buttons
+var buttonsArray = document.querySelectorAll("button");
+
+buttonsArray.forEach(function (element, index) {
+    element.textContent = question.questions[index];
+    element.addEventListener("click", function () {
+        console.log("BUtton CLicKED");
+        if (question.correctAnswer == index) {
+            correctAnswer();
+        } else {
+            wrongAnswer();
+        }
+    });
+});
+
+
+
+
+
+
+
+
+
+};
+
+var correctAnswer = function () {
+    console.log("Correct");
+    currentQuestion++;
+    nextQuestion(questionsArr[currentQuestion]);
+};
+
+var wrongAnswer = function () {
+    console.log("wrong");
+    currentQuestion++;
+    nextQuestion(questionsArr[currentQuestion]);
+};
 
 
 // start quiz button
@@ -57,7 +139,35 @@ btn.type = "Start Quiz";
 btn.name = "formBtn";
 document.body.appendChild(btn);
 
-btn.addEventListener("click", promptUser);
+btn.addEventListener("click", startQuiz);
+
+
+
+
+//timer
+// var secondsLeft = 10;
+
+// function setTime() {
+//     // Sets interval in variable
+//     var timerInterval = setInterval(function () {
+//         secondsLeft--;
+//         timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
+
+//         if (secondsLeft === 0) {
+//             // Stops execution of action at set interval
+//             clearInterval(timerInterval);
+//             // Calls function to create and append image
+//             sendMessage();
+//         }
+//     }, 1000);
+// }
+// function sendMessage() {
+//     timeEl.textContent = " ";
+//     var imgEl = document.createElement("img");
+//     imgEl.setAttribute("src", "images/image_1.jpg");
+//     mainEl.appendChild(imgEl);
+// }
+
 
 
 
